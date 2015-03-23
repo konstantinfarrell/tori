88977dd7d31044a57af785f203bae0a2b86e042b
Konstantin <kfarr2@pdx.edu>
2015-03-22 18:28:20 -0700

    Add keyboard logic

--- tori/ipa/models.py
+++ tori/ipa/models.py
@@ -1,3 +1,12 @@
+import os, sys
 from django.db import models
 
 # Create your models here.
+class Character(models.Model):
+
+    char_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    code = models.CharField(max_length=40, unique=True)
+
+    class Meta:
+        db_table = "character"
--- tori/ipa/views.py
+++ tori/ipa/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Character
 
 def ipa(request):
-    return render(request, 'ipa/ipa.html', {})
+    characters = Character.objects.all()
+    return render(request, 'ipa/ipa.html', {
+        'characters': characters,
+    })
+
+def create(request):
+    return _edit(request, char_id=None)
+
+def edit(request, char_id):
+    return _edit(request, char_id)
+
+def _edit(request, char_id):
+    pass
