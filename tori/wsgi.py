8557a4bb6a8137990fb6260bb45b1e528c77d5fb
root <root@dargue.priviness>
2015-03-28 12:17:51 -0700

    Modify wsgi.py

--- tori/wsgi.py
+++ tori/wsgi.py
@@ -1,3 +1,4 @@
+
 """
 WSGI config for tori project.
 
@@ -8,6 +9,21 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import site
+import sys
+
+prev_sys_path = list(sys.path)
+root = os.path.normpath(os.path.join(os.path.dirname(__file__), "../"))
+sys.path.append(root)
+site.addsitedir(os.path.join(root, ".env/lib/python3.3/site-packages"))
+
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tori.settings")
 
 from django.core.wsgi import get_wsgi_application
