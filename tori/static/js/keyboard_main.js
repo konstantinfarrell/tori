d4a45a57f67e743be30600b39ce217606e3c9bbd
root <root@dude.fatalisms>
2015-04-25 23:50:37 -0700

    Add mostly working keybindings

--- tori/static/js/keyboard_main.js
+++ tori/static/js/keyboard_main.js
@@ -1,50 +1,95 @@
 $(document).ready(function(e){
+    const SHIFT = 16;
 
-    // Instantiate the mode
+    // Initialize the variables
     var ipa_mode = false;
     var last_key_pressed = null;
+    var current_key = null;
+    var times = 0;
     var counter = 0;
+
+    // Get a JSON object with the english keybindings.
+    var bindings = JSON.stringify(english_keybindings);
 
     // Functions to be performed on keydown
     $(document).keydown(function(e){
         if(e.shiftKey){
             ipa_mode = true;
+            $("#ipa-indicator").css("color", "yellow");
+            $("#ipa-indicator").text(" on");
         }
     });
 
     // Functions to be performed on keyup
     $(document).keyup(function(e){
-        
-        if(e.which == 16){
+        if(e.which == SHIFT){
             ipa_mode = false;
+            $("#ipa-indicator").css("color", "white");
+            $("#ipa-indicator").text("off");
         }
     });
 
-    //#TODO: just a few more to go
-    // Functions to be performed on keypress
+    // Functions to be performed on keypress. Mostly keybinding stuff.
     $(document).keypress(function(e){
+
+        // Get the current value of the text input
+        var current_value = $("#ipa-input").val();
+
+        // Which key was just pressed?
+        current_key = String.fromCharCode(e.which);
+        // Initialize the empty list of keys bound to current
+        var bound_to_current = [];
+        bound_to_current.push(current_key);
+        bound_to_current.push(current_key.toLowerCase());
+
         // IPA mode is on. Cycle through them choices
         if(ipa_mode === true){
-            var current_key = String.fromCharCode(e.which);
-            var bound_to_current = [];
-            var bindings = JSON.stringify(english_keybindings);
+
+            // Prevent it from appending the originally pushed key
+            e.preventDefault();
+
+            // Make a list. Maybe check it more than once.
             $.each($.parseJSON(bindings), function(idx, obj){
                 if(obj.key_binding == current_key.toLowerCase()){
                     bound_to_current.push(obj.code);
                 }
             });
-            if(current_key != last_key_pressed){
+
+            // Cycle through the choices.
+            if(current_key === last_key_pressed){
+                counter++;
+                var which = 1;
+                var second_to_last = current_value[current_value.length-2];
+                //#TODO: Figure out this last bug and you're done.
+                if(second_to_last === current_key.toLowerCase() && times < 2){
+                    which = 2;
+                    times++;
+                }
+                current_value = current_value.substring(0, current_value.length - which);
+            } else {
                 counter = 0;
             }
-            e.which = htmlDecode(bound_to_current[counter % bound_to_current.length]);
-            var input = $("#ipa-input").val() + htmlDecode(bound_to_current[counter % bound_to_current.length]);
-            $("#ipa-input").val(input);
-            last_key_pressed = current_key;
-            counter++;
-            alert($("#ipa-input").val());
+
+            // Set the new character to the rendered form of the escaped HTML
+            // indicated by the counter.
+            var char_to_add = htmlDecode(bound_to_current[counter % bound_to_current.length]);
+
+            // Handle an unbound key.
+            if(char_to_add == "undefined"){
+                char_to_add = "";
+            }
+
+            // Rewrite the textarea.
+            $("#ipa-input").val(current_value + char_to_add);
         }
+
+        // Keep track of which key was pressed
+        last_key_pressed = current_key;
     });
 
+    // WOW OK JQUERY. Making me write a function to render the HTML special
+    // character in a div, then return the extracted form on its own. Why isn't
+    // this a feature? huh??
     function htmlDecode(input){
         var e = document.createElement('div');
         e.innerHTML = input;
@@ -65,6 +110,7 @@ $(document).ready(function(e){
         alert(userIn);
     });
 
+    //#TODO: find a way to reset all variables
     // Clear everything
     $("#clear").click(function(e){
         e.preventDefault();
@@ -96,9 +142,9 @@ $(document).ready(function(e){
         $.cookie("whichButton", $(button).attr('id'));
     }
 
-
     // To be run when the page loads
     var button = document.getElementById($.cookie("whichButton"));
     toggleButton($(button));
     toggleKeyboard($(button));
+
 });
